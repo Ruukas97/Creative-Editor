4f50f869800adde3486f7d295eb15a6ccfe0750b
Lucas Olai Jarlkov Olsen <lucas-olai@hotmail.com>
2020-01-17 20:34:52 +0100

    Render Screenshot

--- src/main/java/creativeeditor/screen/ScreenshotScreen.java
+++ src/main/java/creativeeditor/screen/ScreenshotScreen.java
@@ -1,25 +1,31 @@
 package creativeeditor.screen;
 
+import javax.annotation.Nonnull;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+
 import creativeeditor.util.ColorUtils.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.ScreenshotEvent;
+import net.minecraftforge.fml.client.config.GuiUtils;
 
 public class ScreenshotScreen extends Screen {
 
-	private ScreenshotEvent e;
-	@SuppressWarnings("unused")
 	private boolean enableAnimation;
-	private Minecraft mc;
+	private DynamicTexture dyntex;
+	private ResourceLocation screenshotLocation = new ResourceLocation("creativeeditor", "screenshot");
 
-	public ScreenshotScreen(ScreenshotEvent event) {
-		super(null);
+	public ScreenshotScreen(@Nonnull ScreenshotEvent event) {
+		super(new TranslationTextComponent("gui.screenshot"));
 		if (event != null) {
-			this.e = event;
-			this.mc = Minecraft.getInstance();
+			dyntex = new DynamicTexture(event.getImage());
+		} else {
+			onClose();
 		}
-
 	}
 
 	int opacity = 252;
@@ -29,15 +35,26 @@ public class ScreenshotScreen extends Screen {
 	int x2;
 	int y;
 	int y2;
-	
-	
+
 	@Override
-	public void init(Minecraft p_init_1_, int p_init_2_, int p_init_3_) {
-		// Calculate goal
+	public void init(Minecraft mc, int width, int height) {
+		//int i = mc.mainWindow.calcGuiScale(1, false);
+		//mc.mainWindow.setGuiScale(i);
+		super.init(mc, mc.mainWindow.getScaledWidth(), mc.mainWindow.getScaledHeight());
+		minecraft.textureManager.loadTexture(screenshotLocation, dyntex);
 	}
 
 	@Override
-	public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
+	public void onClose() {
+		super.onClose();
+		dyntex.close();
+		minecraft.textureManager.deleteTexture(screenshotLocation);
+		minecraft.updateWindowSize();
+	}
+
+	@Override
+	public void render(int mouseX, int mouseY, float partialTicks) {
+		super.render(mouseX, mouseY, partialTicks);
 		if (opacity > 7) {
 			Color color = new Color(opacity, 255, 255, 255);
 			fill(0, 0, width, height, color.getInt());
@@ -46,19 +63,16 @@ public class ScreenshotScreen extends Screen {
 			} else {
 				hold--;
 			}
+		} else {
+			displayScreen();
 		}
-
 	}
 
 	public void displayScreen() {
-
-		// needs delay but shouldnt be new thread
-		DynamicTexture dyntex = new DynamicTexture(e.getImage());
-		mc.textureManager.loadTexture(null, dyntex);
-		dyntex.bindTexture();
-		mc.ingameGUI.blit(59, 50, 50, 50, 20, 20);
-		dyntex.close();
-
+        minecraft.getTextureManager().bindTexture(screenshotLocation);
+        GlStateManager.enableBlend();
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GuiUtils.drawInscribedRect(50, 50, width-100, height-100, dyntex.getTextureData().getWidth(), dyntex.getTextureData().getHeight());
 	}
 
 	public void animationEnabled(boolean bool) {
