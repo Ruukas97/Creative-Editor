9f1f72b0e3e3a455307fb8851eae3bdd54547414
Thieme <Thieme@hoogernet.nl>
2020-01-17 16:29:58 +0100

    Screenshot animation

--- src/main/java/creativeeditor/eventhandlers/ScreenshotHandler.java
+++ src/main/java/creativeeditor/eventhandlers/ScreenshotHandler.java
@@ -1,13 +1,16 @@
 package creativeeditor.eventhandlers;
 
+import creativeeditor.screen.ScreenshotScreen;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ScreenshotEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ScreenshotHandler {
 
+	
 	@SubscribeEvent
 	public void onScreenShot(ScreenshotEvent e) {
-		//new ScreenshotScreen(e);
+		Minecraft.getInstance().displayGuiScreen(new ScreenshotScreen(e));
 		
 	}
 }
--- src/main/java/creativeeditor/screen/ScreenshotScreen.java
+++ src/main/java/creativeeditor/screen/ScreenshotScreen.java
@@ -1,15 +1,12 @@
 package creativeeditor.screen;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
+import creativeeditor.util.ColorUtils.Color;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.client.renderer.texture.NativeImage;
 import net.minecraftforge.client.event.ScreenshotEvent;
 
-public class ScreenshotScreen {
+public class ScreenshotScreen extends Screen {
 
 	private ScreenshotEvent e;
 	@SuppressWarnings("unused")
@@ -17,6 +14,7 @@ public class ScreenshotScreen {
 	private Minecraft mc;
 
 	public ScreenshotScreen(ScreenshotEvent event) {
+		super(null);
 		if (event != null) {
 			this.e = event;
 			this.mc = Minecraft.getInstance();
@@ -24,30 +22,51 @@ public class ScreenshotScreen {
 
 	}
 
-	public void displayScreen() {
-		mc.execute(() -> {
-			// needs delay but shouldnt be new thread
-			File file = e.getScreenshotFile();
-			try {
-				InputStream targetStream;
-				targetStream = new FileInputStream(file);
-				NativeImage ni = NativeImage.read(targetStream);
-				DynamicTexture dyntex = new DynamicTexture(ni);
-				mc.textureManager.loadTexture(null, dyntex);
-				dyntex.bindTexture();
+	int opacity = 252;
+	int hold = 20;
+	double speed = 0.95;
+	int x;
+	int x2;
+	int y;
+	int y2;
+	
+	
+	@Override
+	public void init(Minecraft p_init_1_, int p_init_2_, int p_init_3_) {
+		// Calculate goal
+	}
 
-				mc.ingameGUI.blit(59, 50, 50, 50, 20, 20);
-				dyntex.close();
-			} catch (Exception e1) {
-				e1.printStackTrace();
+	@Override
+	public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
+		if (opacity > 7) {
+			Color color = new Color(opacity, 255, 255, 255);
+			fill(0, 0, width, height, color.getInt());
+			if (hold <= 0) {
+				opacity -= 7;
+			} else {
+				hold--;
 			}
-		});
-		
+		}
 
 	}
 
-	public void shouldPlayAnimation(boolean bool) {
+	public void displayScreen() {
+
+		// needs delay but shouldnt be new thread
+		DynamicTexture dyntex = new DynamicTexture(e.getImage());
+		mc.textureManager.loadTexture(null, dyntex);
+		dyntex.bindTexture();
+		mc.ingameGUI.blit(59, 50, 50, 50, 20, 20);
+		dyntex.close();
+
+	}
+
+	public void animationEnabled(boolean bool) {
 		this.enableAnimation = bool;
 	}
 
+	@Override
+	public boolean isPauseScreen() {
+		return false;
+	}
 }
