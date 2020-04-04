214ec2d7dda2ccd0b909cc60dddec5f89199398e
Lucas <lucas-olai@hotmail.com>
2020-04-04 19:32:09 +0200

    "Air" stuff

--- src/main/java/creativeeditor/screen/ParentItemScreen.java
+++ src/main/java/creativeeditor/screen/ParentItemScreen.java
@@ -9,7 +9,9 @@ import creativeeditor.widgets.StyledButton;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.text.ITextComponent;
 
 public class ParentItemScreen extends ParentScreen {
@@ -52,10 +54,12 @@ public class ParentItemScreen extends ParentScreen {
                 mc.displayGuiScreen( new MainScreen( this.lastScreen, new DataItem( item.getItem().getItem(), 1, null, item.getSlot().get() ) ) );
             } ) );
             addButton( new StyledButton( posX, posY + 10, bwidth, 20, I18n.format( "gui.main.save" ), b -> {
-                mc.playerController.sendSlotPacket( item.getItemStack(), 36 + mc.player.inventory.currentItem );
+                if (item.getItem().getItem() != Items.AIR)
+                    mc.playerController.sendSlotPacket( item.getItemStack(), 36 + mc.player.inventory.currentItem );
             } ) );
             addButton( new StyledButton( posX + bwidth + 1, posY, bwidth, 20, I18n.format( "gui.main.drop" ), b -> {
-                mc.playerController.sendPacketDropItem( item.getItemStack() );
+                if (item.getItem().getItem() != Items.AIR)
+                    mc.playerController.sendPacketDropItem( item.getItemStack() );
                 // Shift for /give
             } ) );
         }
@@ -86,15 +90,24 @@ public class ParentItemScreen extends ParentScreen {
         // Item (Tooltip must render last or colors will be messed up)
         if (renderItem) {
             ItemStack stack = item.getItemStack();
-            GlStateManager.scalef( itemScale, itemScale, 1f );
-            RenderHelper.enableGUIStandardItemLighting();
-            int x = (int) (width / (2 * itemScale) - 8);
-            int y = (int) (30 / itemScale + height / (2 * itemScale) - 8);
-            itemRenderer.renderItemIntoGUI( stack, x, y );
-            itemRenderer.renderItemOverlayIntoGUI( font, item.getItemStack(), x, y, null );
-            RenderHelper.disableStandardItemLighting();
 
-            GlStateManager.scalef( 1f / itemScale, 1f / itemScale, 1f );
+            Item ite = item.getItem().getItem();
+            if (ite == Items.AIR) {
+                int x = (int) (width / 2);
+                int y = (int) (30 + height / (2) - 5);
+                drawCenteredString( font, ite.getDisplayName( stack ).getFormattedText(), x, y, -1 );
+            }
+            else {
+                GlStateManager.scalef( itemScale, itemScale, 1f );
+                RenderHelper.enableGUIStandardItemLighting();
+                int x = (int) (width / (2 * itemScale) - 8);
+                int y = (int) (30 / itemScale + height / (2 * itemScale) - 8);
+                itemRenderer.renderItemIntoGUI( stack, x, y );
+                itemRenderer.renderItemOverlayIntoGUI( font, item.getItemStack(), x, y, null );
+                RenderHelper.disableStandardItemLighting();
+
+                GlStateManager.scalef( 1f / itemScale, 1f / itemScale, 1f );
+            }
 
             // Item frame
             GuiUtil.drawFrame( width / 2 - 20, height / 2 + 10, width / 2 + 20, height / 2 + 50, 1, color );
