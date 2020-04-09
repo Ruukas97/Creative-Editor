0602c540500471b15b5498a6bbf84aef710ddbd1
Lucas <lucas-olai@hotmail.com>
2020-04-09 18:13:00 +0200

    Added armorstands with arms and no base plate to Unavailable

--- src/main/java/creativeeditor/tab/TabUnavailable.java
+++ src/main/java/creativeeditor/tab/TabUnavailable.java
@@ -1,8 +1,11 @@
 package creativeeditor.tab;
 
+import creativeeditor.data.DataItem;
+import creativeeditor.data.tag.entity.TagEntityArmorStand;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.registries.GameData;
 
@@ -21,13 +24,20 @@ public class TabUnavailable extends TabCreative {
 
     @Override
     public void fill( NonNullList<ItemStack> items ) {
-        // super.fill(items); super sees if any items are assigned to this tab - this
-        // tab by definition list items that aren't assigned any tabs
-
         GameData.getWrapper( Item.class ).forEach( i -> {
             if (i != Items.AIR && (i.getCreativeTabs().isEmpty() || (i.getCreativeTabs().size() == 1 && i.getCreativeTabs().contains( null )))) {
                 items.add( new ItemStack( i ) );
             }
         } );
+        
+        DataItem stand = new DataItem( Items.ARMOR_STAND, 1, new CompoundNBT(), 0 );
+        TagEntityArmorStand data = stand.getTag().getArmorStandTag();
+        
+        data.getShowArms().set( true );
+        items.add( stand.getData() );
+        data.getNoBasePlate().set( true );
+        items.add( stand.getData() );
+        data.getShowArms().set( false );
+        items.add( stand.getData() );
     }
 }
