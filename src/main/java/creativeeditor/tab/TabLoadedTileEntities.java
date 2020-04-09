477aae8688404f0abb7e98e671041dd5712ef713
Lucas <lucas-olai@hotmail.com>
2020-04-09 15:27:18 +0200

    Added armorstands to TEs tab

--- src/main/java/creativeeditor/tab/TabLoadedTileEntities.java
+++ src/main/java/creativeeditor/tab/TabLoadedTileEntities.java
@@ -1,12 +1,16 @@
 package creativeeditor.tab;
 
 import creativeeditor.data.DataItem;
+import creativeeditor.data.version.NBTKeys;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ArmorStandEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.NonNullList;
 
@@ -27,18 +31,47 @@ public class TabLoadedTileEntities extends TabCreative {
             DataItem dItem = new DataItem( item, 1, te, 0 );
             ItemStack stack = dItem.getData();
             boolean found = false;;
-            for(ItemStack ex : items) {
-                if(ex.equals( stack, false )) {
+            for (ItemStack ex : items) {
+                if (ex.equals( stack, false )) {
                     found = true;
-                    if(ex.getCount() < 64)
+                    if (ex.getCount() < 64)
                         ex.setCount( ex.getCount() + 1 );
                     break;
                 }
             }
-            if(!found) {
+            if (!found) {
                 items.add( stack );
             }
         }
+
+        NonNullList<ItemStack> stands = NonNullList.create();
+        for (Entity ent : world.getAllEntities()) {
+            if (ent instanceof ArmorStandEntity) {
+                ArmorStandEntity stand = (ArmorStandEntity) ent;
+                CompoundNBT itemTag = new CompoundNBT();
+                CompoundNBT entityTag = new CompoundNBT();
+                stand.writeAdditional( entityTag );
+                itemTag.put( NBTKeys.keys.tagEntityTag(), entityTag );
+                
+                DataItem dItem = new DataItem( Items.ARMOR_STAND, 1, itemTag, 0 );
+                
+                ItemStack stack = dItem.getData();
+                boolean found = false;;
+                for (ItemStack ex : stands) {
+                    if (ex.equals( stack, false )) {
+                        found = true;
+                        if (ex.getCount() < 64)
+                            ex.setCount( ex.getCount() + 1 );
+                        break;
+                    }
+                }
+                if (!found) {
+                    stands.add( stack );
+                }
+            }
+        }
+        
+        items.addAll( stands );
     }
 
 
