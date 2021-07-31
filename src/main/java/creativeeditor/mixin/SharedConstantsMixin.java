ca153bebe14bd6d8664eb74dc2be5f56059e1199
Thieme <thieme@hoogernet.nl>
2021-08-01 00:00:14 +0200

    Allowing to use the section sign (§) everywhere

---
+++ src/main/java/creativeeditor/mixin/SharedConstantsMixin.java
@@ -0,0 +1,15 @@
+package creativeeditor.mixin;
+
+import net.minecraft.util.SharedConstants;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin({SharedConstants.class})
+public class SharedConstantsMixin {
+
+    @Overwrite
+    public static boolean isAllowedChatCharacter(char p) { //func_71566_a
+        return (p >= ' ' && p != ' ');
+    }
+
+}
