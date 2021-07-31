7c7f753a9936ccad56148624176893244c49a427
Thieme <thieme@hoogernet.nl>
2021-08-01 00:00:14 +0200

    Allowing to use the section sign (§) everywhere

---
+++ src/main/java/creativeeditor/mixin/TextFormattingMixin.java
@@ -0,0 +1,16 @@
+package creativeeditor.mixin;
+
+import net.minecraft.util.text.TextFormatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import javax.annotation.Nullable;
+
+@Mixin({TextFormatting.class})
+public class TextFormattingMixin {
+
+    @Overwrite
+    public static String stripFormatting(@Nullable String string) { // func_110646_a
+        return string == null ? null : string;
+    }
+}
