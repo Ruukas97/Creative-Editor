3c9fba66466993bf40fa53796f0e19223671cad5
Thieme Hoogerwerf <Thieme@hoogernet.nl>
2021-08-15 08:16:16 +0200

    Fixed mixins, now also generates and includes refmap for build

--- src/main/java/creativeeditor/mixin/SharedConstantsMixin.java
+++ src/main/java/creativeeditor/mixin/SharedConstantsMixin.java
@@ -7,6 +7,10 @@ import org.spongepowered.asm.mixin.Overwrite;
 @Mixin({SharedConstants.class})
 public class SharedConstantsMixin {
 
+    /**
+     * @author ThiemeH
+     * @reason Removes the section sign from invalid chat character
+     */
     @Overwrite
     public static boolean isAllowedChatCharacter(char p) { //func_71566_a
         return (p >= ' ' && p != '');
--- src/main/java/creativeeditor/mixin/TextFormattingMixin.java
+++ src/main/java/creativeeditor/mixin/TextFormattingMixin.java
@@ -9,6 +9,10 @@ import javax.annotation.Nullable;
 @Mixin({TextFormatting.class})
 public class TextFormattingMixin {
 
+    /**
+     * @author ThiemeH
+     * @reason Not strip with the strip formatting
+     */
     @Overwrite
     public static String stripFormatting(@Nullable String string) { // func_110646_a
         return string;
