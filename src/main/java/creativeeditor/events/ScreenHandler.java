01d038b4c5d6fbba203eb6ba421d0738ff22460c
Thieme <thieme@hoogernet.nl>
2021-08-31 22:05:29 +0200

    Prioritized ColorHelperWidget in injections

--- src/main/java/creativeeditor/events/ScreenHandler.java
+++ src/main/java/creativeeditor/events/ScreenHandler.java
@@ -1,6 +1,8 @@
 package creativeeditor.events;
 
+import creativeeditor.screen.ParentItemScreen;
 import creativeeditor.screen.widgets.ColorHelperWidget;
+import net.minecraft.client.gui.IGuiEventListener;
 import net.minecraft.client.gui.screen.AddServerScreen;
 import net.minecraft.client.gui.screen.EditBookScreen;
 import net.minecraft.client.gui.screen.EditSignScreen;
@@ -10,10 +12,22 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ScreenHandler {
 
+    // Add ColorHelperWidget to GUI's
     @SubscribeEvent()
     public void onInitGui(GuiScreenEvent.InitGuiEvent e) {
         if (e.getGui() instanceof EditSignScreen || e.getGui() instanceof EditWorldScreen || e.getGui() instanceof EditBookScreen || e.getGui() instanceof AddServerScreen) {
             e.addWidget(new ColorHelperWidget(null, 156, 30, e.getGui().width, e.getGui().height));
+        }
+    }
+
+    // Prioritize ColorHelperWidget
+    @SubscribeEvent()
+    public void onClick(GuiScreenEvent.MouseClickedEvent.Pre e) {
+        if (e.getGui() instanceof ParentItemScreen) return;
+        for (IGuiEventListener gel : e.getGui().children()) {
+            if (gel instanceof ColorHelperWidget) {
+                gel.mouseClicked(e.getMouseX(), e.getMouseY(), e.getButton());
+            }
         }
     }
 
